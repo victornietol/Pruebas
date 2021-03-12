67c74cc34f9d9de4878c0a1239a9a9d09bf306b8
Victor Nieto <victornieto315@aragon.unam.mx>
2021-03-12 17:26:54 -0600

    12_marzo_2021

---
+++ 12_marzo_2021/proyecto_inicial_poo/src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
@@ -0,0 +1,64 @@
+
+package mx.unam.aragon.fes.persistencia;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import mx.unam.aragon.fes.Empleado;
+
+
+public class ArchivoEmpleado {
+    private String archivo;
+
+    public ArchivoEmpleado() {
+    }
+
+    public ArchivoEmpleado(String archivo) {
+        this.archivo = archivo;
+    }
+
+    public String getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(String archivo) {
+        this.archivo = archivo;
+    }
+    
+    public void guardarEmpleados(ArrayList<Empleado> datos){
+        // aqui el codigo para grabar en Disco duro
+        try {
+            ObjectOutputStream fSalida = new ObjectOutputStream(
+                    new FileOutputStream(archivo));
+
+            fSalida.writeObject(datos);
+            fSalida.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public ArrayList<Empleado> leerEmpleados(){
+        ArrayList<Empleado> tmp=null;
+        
+         try {
+             ObjectInputStream fLectura = new ObjectInputStream(
+                    new FileInputStream(archivo));
+
+            tmp = (ArrayList<Empleado>) fLectura.readObject();
+            if (tmp == null) {
+                System.out.println("No hay nada");
+            } else {
+                System.out.println("Nombre" + tmp.size());
+            }
+            fLectura.close();
+
+        } catch (Exception ex) {
+            System.out.println("Error" + ex.toString());
+        }
+        return tmp;
+    }
+    
+}
