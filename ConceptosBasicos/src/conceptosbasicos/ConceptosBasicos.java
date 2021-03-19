e4e332d7a317bcbd1f0c8da4ddf4c666f6d0233c
Victor Nieto <victornieto315@aragon.unam.mx>
2021-03-19 17:25:46 -0600

    19_marzo_2021

---
+++ ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conceptosbasicos;
+
+import ico.fes.Automovil;
+import java.awt.Color;
+
+/**
+ *
+ * @author Victor
+ */
+public class ConceptosBasicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Automovil miBocho= new Automovil();
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.blue);
+        System.out.println( miBocho);
+        
+        //TAREA. Programar los objetos miAcura y miMustang
+    }
+    
+}
